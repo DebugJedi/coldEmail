8e78540a42f43508b341810717cde943dec90582
DebugJedi <priyankrao2512@gmail.com>
2024-09-30 11:48:21 -0400

    updated css

--- app/streamlit_email_app.py
+++ app/streamlit_email_app.py
@@ -3,29 +3,12 @@ from emailGenerator import E_generator
 from PIL import Image
 import streamlit_shadcn_ui as ui
 
-# trigger_btn = ui.button(text="Trigger Button", key='trigger_btn')
 
-# ui.alert_dialog(
-#     show=trigger_btn,
-#     title="Alert Dialog",
-#     description="This is an alert dialog",
-#     confirm_label="OK",
-#     cancel_label="Cancel",
-#     key="alert_dialog1",
-
-# )
-
-
-# col1, col2, col3 = st.columns(3)
-# with col1:
-#     st.write(' ')
-# with col2:
 with open("assets/style.css") as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 img = Image.open('app/resources/photos/Email-Generator.jpg')
 st.image(img)
-# with col3:
-#     st.write(' ')
+
 
 st.write("<span class = 'header email_app'>📧 Email Generator</span>", unsafe_allow_html=True)
 jobPosting = st.text_input("Enter a job URL:", value = "https://boards.greenhouse.io/benchling/jobs/6270990" )
