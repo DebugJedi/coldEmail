4501222a652cf510db9a59b60052b8709ae3b7d7
DebugJedi <priyankrao2512@gmail.com>
2024-09-29 22:55:42 -0400

    added css

--- app/streamlit_email_app.py
+++ app/streamlit_email_app.py
@@ -20,6 +20,8 @@ import streamlit_shadcn_ui as ui
 # with col1:
 #     st.write(' ')
 # with col2:
+with open("assets/style.css") as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 img = Image.open('app/resources/photos/Email-Generator.jpg')
 st.image(img)
 # with col3:
@@ -29,9 +31,11 @@ st.markdown("## 📧 Email Generator")
 jobPosting = st.text_input("Enter a job URL:", value = "https://boards.greenhouse.io/benchling/jobs/6270990" )
 submit_button = st.button("Submit")
 
+
 if submit_button:
 
     email = E_generator(jobPosting)
     generated_email = email.run()
     st.write(generated_email)
 
+    
