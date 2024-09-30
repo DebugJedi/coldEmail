e4991f8ec476f98698eb0c95ce59215cb0333eb0
DebugJedi <priyankrao2512@gmail.com>
2024-09-30 17:51:06 -0400

    testing

--- app/streamlit_email_app.py
+++ app/streamlit_email_app.py
@@ -17,7 +17,9 @@ doc_file = st.file_uploader("Upload your resume/document",
 if st.button("Upload"):
     
     extractor_func = extract_resume()
+    st.spinner(text="uploading...")
     document = extractor_func.load(doc_file)
+    st.write(document)
     
 
 jobPosting = st.text_input("Enter a job URL:", value = "https://boards.greenhouse.io/benchling/jobs/6270990" )
@@ -26,7 +28,6 @@ submit_button = st.button("Submit")
 with open("assets/style.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 if submit_button:
-        
     email = E_generator(jobPosting, document)
     generated_email = email.run()
     st.write("<span class = 'ai generated_email'>{}</span>".format(generated_email), unsafe_allow_html=True)
