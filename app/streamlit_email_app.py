dc7d83bf9752bdde763aecadbb8a20b798ad0274
DebugJedi <priyankrao2512@gmail.com>
2024-09-30 23:56:10 -0400

    design update

--- app/streamlit_email_app.py
+++ app/streamlit_email_app.py
@@ -15,8 +15,8 @@ st.image(img)
 st.write("<span class = 'header email_app'>📧 Email Generator</span>", unsafe_allow_html=True)
 
 # Document upload
-st.markdown("***Resume/CV (PDF)***")
-doc_file = st.file_uploader("",
+# st.markdown("******")
+doc_file = st.file_uploader("Resume/CV (PDF)",
                             type=["pdf"])
 
 jobPosting = st.text_input("Enter a job URL:", value = "https://boards.greenhouse.io/benchling/jobs/6270990" )
